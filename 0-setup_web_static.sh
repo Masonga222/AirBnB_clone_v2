11dc6834b93dd21a68cf9efbabd1d87a661472a9
Masonga222 <masongashemap@gmail.com>
2024-03-10 05:22:06 -0700

    AirBnB_clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
-# Sets up your web servers for the deployment of web_static
-apt-get -y update
-apt-get -y install nginx
-service nginx start
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-touch /data/web_static/releases/test/index.html
-echo "Simple content, to test the configuration" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-file='/etc/nginx/sites-available/default'
-line=56
-text='\t}\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}'
-sed -i "${line}s~.*~${text}~" $file
-service nginx restart
-exit 0
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,33 +1,23 @@
 #!/usr/bin/python3
 """
-Python script to generate a .tgz archeive from the web_static files
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
 """
 
-from fabric.api import local
 from datetime import datetime
-import os
+from fabric.api import *
 
 
 def do_pack():
     """
-    Function to create a .tgz archive from the web_static folder
+    making an archive on web_static folder
     """
 
-    # creating version folder if not exsists
-    local(" mkdir -p versions/")
-
-    # defining archeive name
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    arc_name = f"web_static_{timestamp}.tgz"
-
-    result = local("tar -czvf versions/{} web_static".format(arc_name))
-
-    if result:
-        return "versions/{}".format(arc_name)
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
     else:
         return None
-
-
-# calling the script do_pack function when the script is run
-if __name__ == "__main__":
-    do_pack()
